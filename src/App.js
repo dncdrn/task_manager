d2499e626db11d92aa62c3008127d9da3f28a583
dncdrn <missnentin@gmail.com>
2021-06-25 16:47:51 +0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import routes from "./routes/index";
-import { loginAccount } from "./service/loginAPI";
-import { getDashboardData } from "./service/dashboardAPI";
-import {
-  getAllTasks,
-  addTask,
-  updateTask,
-  deleteTask,
-} from "./service/taskAPI";
-
+import Login from "./Page/Login/Login";
+import "antd/dist/antd.css";
+import { StyledDiv } from "./AppStyle";
 function App() {
-  useEffect(() => {
-    (async function () {
-      const x = await loginAccount({
-        name: "Danica Durana",
-        apiKey: process.env.REACT_APP_API_KEY,
-      });
-    })();
-    (async function () {
-      const x = await getDashboardData();
-      console.log("getDashboardData", x);
-    })();
-    // (async function () {
-    //   const x = await deleteTask("60d57b9db67e24277b6f00d6");
-    //   console.log("deleteTask", x);
-    // })();
-    // (async function () {
-    //   const x = await addTask({ name: "Create login" });
-    //   console.log("addTask", x);
-    // })();
-    // (async function () {
-    //   const x = await updateTask("60d57b9db67e24277b6f00d6", {
-    //     name: "Create Login Component",
-    //   });
-    //   console.log("updateTask", x);
-    // })();
-  }, []);
-  return <h1>heloo</h1>;
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
+  return (
+    <StyledDiv>
+      <Router>
+        {routes.map(({ title, component: Component, url, exact }) => {
+          return (
+            <Route
+              key={url}
+              path={url}
+              exact={exact}
+              render={(compProps) => <Component {...compProps} title={title} />}
+            />
+          );
+        })}
+      </Router>
+    </StyledDiv>
+  );
 }
 
 export default App;
