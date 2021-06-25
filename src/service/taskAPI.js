8ca0e96a4cea156b0c0d6927d41bfb3844faf108
dncdrn <missnentin@gmail.com>
2021-06-25 14:54:30 +0800

    added task API service

---
+++ src/service/taskAPI.js
@@ -0,0 +1,14 @@
+import { authAxios } from "./authSetup";
+
+export function getAllTasks() {
+  return authAxios.get(`/tasks`).then((res) => res.data);
+}
+export function addTask(taskData) {
+  return authAxios.post(`/tasks`, taskData).then((res) => res.data);
+}
+export function updateTask(id, taskData) {
+  return authAxios.put(`/tasks/${id}`, taskData).then((res) => res.data);
+}
+export function deleteTask(id) {
+  return authAxios.delete(`/tasks/${id}`).then((res) => res.data);
+}
