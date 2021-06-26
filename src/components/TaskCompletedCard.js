ac09034ddfef185b3a342e795bc623ba7f7fe187
dncdrn <missnentin@gmail.com>
2021-06-27 04:29:11 +0800

    added skeleton loader

--- src/components/TaskCompletedCard.js
+++ src/components/TaskCompletedCard.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { Typography, Card } from "antd";
+import { Typography, Card, Skeleton } from "antd";
 const { Title } = Typography;
 
-export default function TaskCompletedCard({ totalTasks, tasksCompleted }) {
+export default function TaskCompletedCard({
+  totalTasks,
+  tasksCompleted,
+  isLoading,
+}) {
   return (
     <Card style={{ width: "400px" }}>
-      <Title level={3}>Tasks Completed</Title>
-
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Title style={{ paddingBottom: "15px", fontSize: "61px" }}>
-          {tasksCompleted}
-        </Title>{" "}
-        <Title level={4}>/{totalTasks}</Title>
-      </div>
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <>
+          <Title level={3}>Tasks Completed</Title>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Title style={{ paddingBottom: "15px", fontSize: "61px" }}>
+              {tasksCompleted}
+            </Title>{" "}
+            <Title level={4}>/{totalTasks}</Title>
+          </div>
+        </>
+      )}
     </Card>
   );
 }
