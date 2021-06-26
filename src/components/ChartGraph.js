747388104b782db23d161c1e4355f9247fc838d6
dncdrn <missnentin@gmail.com>
2021-06-27 04:27:35 +0800

    added card and skeleton as loader

--- src/components/ChartGraph.js
+++ src/components/ChartGraph.js
@@ -1,19 +1,24 @@
+import { Card, Skeleton } from "antd";
 import React from "react";
 import Chart from "react-google-charts";
 
-export default function ChartGraph({ totalTasks, tasksCompleted }) {
+export default function ChartGraph({ totalTasks, tasksCompleted, isLoading }) {
   return (
-    <Chart
-      width={"400px"}
-      height={"200px"}
-      chartType="PieChart"
-      loader={<div>Loading Chart</div>}
-      data={[
-        ["TotalTasks", "TasksCompleted"],
-        ["Number of Tasks", totalTasks],
-        ["Completed Tasks", tasksCompleted],
-      ]}
-      rootProps={{ "data-testid": "1" }}
-    />
+    <Card style={{ width: "400px" }}>
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <Chart
+          chartType="PieChart"
+          // loader={<div>Loading Chart</div>}
+          data={[
+            ["TotalTasks", "TasksCompleted"],
+            ["Number of Tasks", totalTasks],
+            ["Completed Tasks", tasksCompleted],
+          ]}
+          rootProps={{ "data-testid": "1" }}
+        />
+      )}
+    </Card>
   );
 }
