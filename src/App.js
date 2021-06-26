d7148af7cfa46a8d231f6a4118c58844b71ab108
dncdrn <missnentin@gmail.com>
2021-06-27 03:28:35 +0800

    removed the layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from "./Page/Login/LoginPage";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import Dashboard from "./Page/Dashboard";
 import NewTaskPage from "./Page/NewTaskPage";
 import "antd/dist/antd.css";
-import { Layout, Menu } from "antd";
-import Avatar from "antd/lib/avatar/avatar";
-import { domainURL } from "./service/domainURL";
-const { Header, Content } = Layout;
 
 function App() {
   return (
@@ -17,23 +13,8 @@ function App() {
         <Route path="/" exact>
           <LoginPage />
         </Route>
-
-        <Layout className="layout">
-          <Header>
-            <div className="logo"></div>
-            <Menu theme="dark" mode="horizontal">
-              <Avatar
-                size={61}
-                src={domainURL + localStorage.getItem("user_image")}
-              />
-              {/* <Menu.Item key={1}>Task Manager</Menu.Item> */}
-            </Menu>
-          </Header>
-          <Content>
-            <ProtectedRoute path="/dashboard" component={Dashboard} />
-            <ProtectedRoute path="/new_task" component={NewTaskPage} />
-          </Content>
-        </Layout>
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
+        <ProtectedRoute path="/new_task" component={NewTaskPage} />
       </Switch>
     </Router>
   );
