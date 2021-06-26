74af3ccd943154c403563d8d539237ff3dc036b0
dncdrn <missnentin@gmail.com>
2021-06-27 00:23:38 +0800

    added task item

---
+++ src/components/TaskItem.js
@@ -0,0 +1,43 @@
+import { Checkbox, Typography, List } from "antd";
+import React from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { updateTask } from "../service/taskAPI";
+const { Title } = Typography;
+
+export default function TaskItem({
+  taskItem,
+  markTaskCompleted,
+  markTaskDeleted,
+  markTaskToUpdate,
+}) {
+  return (
+    <List.Item
+      key={taskItem._id}
+      actions={[
+        <Title
+          level={4}
+          key="list-loadmore-edit"
+          onClick={() => markTaskToUpdate(taskItem)}
+        >
+          <EditOutlined />
+        </Title>,
+        <Title
+          level={4}
+          key="list-loadmore-more"
+          onClick={() => markTaskDeleted(taskItem._id)}
+        >
+          <DeleteOutlined />
+        </Title>,
+      ]}
+    >
+      <Checkbox
+        checked={taskItem.completed}
+        onChange={() => markTaskCompleted(taskItem)}
+      >
+        <Title level={5} delete={taskItem.completed}>
+          {taskItem.name}
+        </Title>
+      </Checkbox>
+    </List.Item>
+  );
+}
