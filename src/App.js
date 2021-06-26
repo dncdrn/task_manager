2c336f2b6cbc18991d7a51cd5afe4438696dde96
dncdrn <missnentin@gmail.com>
2021-06-26 16:54:11 +0800

    modify routes and added protected route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import routes from "./routes/index";
-import Login from "./Page/Login/Login";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginPage from "./Page/Login/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./Page/Dashboard";
 import "antd/dist/antd.css";
-import { StyledDiv } from "./AppStyle";
+
+const authed = localStorage.getItem("user_token");
+
 function App() {
-  const [token, setToken] = useState();
-
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-
   return (
-    <StyledDiv>
-      <Router>
-        {routes.map(({ title, component: Component, url, exact }) => {
-          return (
-            <Route
-              key={url}
-              path={url}
-              exact={exact}
-              render={(compProps) => <Component {...compProps} title={title} />}
-            />
-          );
-        })}
-      </Router>
-    </StyledDiv>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+        <ProtectedRoute
+          path="/dashboard"
+          component={Dashboard}
+          authed={authed}
+        />
+      </Switch>
+    </Router>
   );
 }
 
