288cf40d9c79b84e2ae7b87906306b7dba2aa47f
dncdrn <missnentin@gmail.com>
2021-06-27 04:28:09 +0800

    added skeleton loader

--- src/components/LatestCreatedCard.js
+++ src/components/LatestCreatedCard.js
@@ -1,22 +1,28 @@
-import { Card, Typography } from "antd";
+import { Card, Skeleton, Typography } from "antd";
 import React from "react";
 const { Text, Title } = Typography;
 
-export default function LatestCreatedCard({ taskList }) {
+export default function LatestCreatedCard({ latestTasks, isLoading }) {
   return (
     <Card style={{ width: "400px" }}>
-      <Title level={3}>Latest Created Tasks</Title>
-      <ul>
-        {taskList.length !== 0 ? (
-          taskList.map((task) => (
-            <li key={task.name}>
-              <Text delete={task.completed && true}>{task.name}</Text>
-            </li>
-          ))
-        ) : (
-          <h5>no new task</h5>
-        )}
-      </ul>
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <>
+          <Title level={3}>Latest Created Tasks</Title>
+          <ul>
+            {latestTasks.length !== 0 ? (
+              latestTasks.map((task) => (
+                <li key={task.name}>
+                  <Text delete={task.completed}>{task.name}</Text>
+                </li>
+              ))
+            ) : (
+              <h5>no new task</h5>
+            )}
+          </ul>
+        </>
+      )}
     </Card>
   );
 }
