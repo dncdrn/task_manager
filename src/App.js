3a84fd4a807de12cbd4e24b2a1d1b8c230dd658a
dncdrn <missnentin@gmail.com>
2021-06-27 15:29:14 +0800

    modify page folder name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LoginPage from "./Page/Login/LoginPage";
-import ProtectedRoute from "./Components/ProtectedRoute";
-import Dashboard from "./Page/Dashboard";
-import NewTaskPage from "./Page/NewTaskPage";
+import LoginPage from "./page/Login/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./page/Dashboard";
+import NewTaskPage from "./page/NewTaskPage";
 import "antd/dist/antd.css";
 
 function App() {
