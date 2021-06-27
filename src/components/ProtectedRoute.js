d43984d985708da30ba90c88ff41140695bb9fbe
dncdrn <missnentin@gmail.com>
2021-06-27 15:33:36 +0800

    removed layout

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,41 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Route, Redirect, useHistory } from "react-router-dom";
-import { Layout, Avatar } from "antd";
-import { domainURL } from "../service/domainURL";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const { Header, Content } = Layout;
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const authed = localStorage.getItem("user_token");
-  const history = useHistory();
-  function handleLogout() {
-    window.localStorage.clear();
-    history.push("/");
-  }
+
   return (
-    <Layout className="layout">
-      <Header>
-        <div className="logo"></div>
-        <Avatar
-          size={61}
-          src={domainURL + localStorage.getItem("user_image")}
-        />
-        <a onClick={handleLogout}>Logout</a>
-      </Header>
-      <Content>
-        <Route
-          {...rest}
-          render={(props) =>
-            authed ? (
-              <div>
-                <Component {...props} />
-              </div>
-            ) : (
-              <Redirect to={{ pathname: "/" }} />
-            )
-          }
-        />
-      </Content>
-    </Layout>
+    <Route
+      {...rest}
+      render={(props) =>
+        authed ? (
+          <div>
+            <Component {...props} />
+          </div>
+        ) : (
+          <Redirect to={{ pathname: "/" }} />
+        )
+      }
+    />
   );
 };
 
