2d6cd8e0774f5fe27d301a2d6b9c23eb5371fc99
dncdrn <missnentin@gmail.com>
2021-06-28 04:29:18 +0800

    added data test id for testing

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,51 +1,43 @@
 import React from "react";
-import { Card, message } from "antd";
+import { Card } from "antd";
 import { loginAccount } from "../service/loginAPI";
 import { Input, Form, Button } from "antd";
 import { useHistory } from "react-router-dom";
 import { SearchOutlined } from "@ant-design/icons";
 import dashboardStyle from "../components/DashboardStyle";
 import Title from "antd/lib/typography/Title";
-
 function LoginPage() {
   let history = useHistory();
-  const [form] = Form.useForm();
   const dashboardClasses = dashboardStyle();
 
   async function onFormSubmit(values) {
     // adding api key to values
-    try {
-      values.apiKey = process.env.REACT_APP_API_KEY;
-      const loginResult = await loginAccount(values);
-      localStorage.setItem("user_token", loginResult.token.token);
-      localStorage.setItem("user_image", loginResult.image);
-      localStorage.setItem("user_name", loginResult.token.name);
-      form.resetFields();
-      history.push("/dashboard");
-    } catch (error) {
-      form.resetFields();
-      message.error(error.response.data.msg);
-    }
+    values.apiKey = process.env.REACT_APP_API_KEY;
+    const loginResult = await loginAccount(values);
+    localStorage.setItem("user_token", loginResult.token.token);
+    localStorage.setItem("user_image", loginResult.image);
+    localStorage.setItem("user_name", loginResult.token.name);
+    history.push("/dashboard");
   }
 
   return (
-    <div className={dashboardClasses.loginContainer}>
+    <div className={dashboardClasses.cardContainer}>
       <Card
         title={<Title level={3}>Login</Title>}
         className={dashboardClasses.card}
       >
-        <Form form={form} onFinish={onFormSubmit}>
+        <Form onFinish={onFormSubmit}>
           <Form.Item name="id">
-            <Input placeholder="Id" />
+            <Input placeholder="Id" data-testid="id-number" />
           </Form.Item>
           <Form.Item name="name">
-            <Input placeholder="Name" />
+            <Input
+              prefix={<SearchOutlined />}
+              placeholder="Name"
+              data-testid="name"
+            />
           </Form.Item>
-          <Button
-            className={dashboardClasses.button}
-            type="primary"
-            htmlType="submit"
-          >
+          <Button type="primary" htmlType="submit" data-testid="login-button">
             Login
           </Button>
         </Form>
